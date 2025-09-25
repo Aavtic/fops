396fffab02a9604fc8a7a050a048f1c5a5ce3631
Aadish M <89965681+Aavtic@users.noreply.github.com>
2025-09-26 01:19:15 +0530

    Added comments for default python template

--- backend/server/utils/templates/python_template.go
+++ backend/server/utils/templates/python_template.go
@@ -5,9 +5,9 @@ import (
 )
 
 func GeneratePythonTemplate(func_name, para_name, para_type, output_type string) string {
-	return fmt.Sprintf(`This project is free and open-source
-please report any issues you find at
-FOPS: https://github.com/aavtic/fops/issues
+	return fmt.Sprintf(`# This project is free and open-source
+	# please report any issues you find at
+	# FOPS: https://github.com/aavtic/fops/issues
 
 class Solution:
     def %s(%s: %s) -> %s:
