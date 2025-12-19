292d4a1b8d6f5197de1af6c1ec96ebd3716909d7
Aadish <aadishmkerala@gmail.com>
2025-12-19 23:57:37 +0530

    fix: #19 refactored go code to idiomatic go structure

--- backend/server/main.go
+++ backend/server/main.go
@@ -2,8 +2,11 @@ package main;
 
 import (
 	"github.com/aavtic/fops/internal/server"
+	"github.com/aavtic/fops/internal/config"
 )
 
 func main() {
+	config := config.NewConfig()
+	server := server.NewServer(config);
 	server.Run()
 }
